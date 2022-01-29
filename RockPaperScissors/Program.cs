a2e10429e151de18b1d42571cc50ea747910c2d4
m-yilmaz <yilmazmustafa93@gmail.com>
2022-01-29 11:22:38 +0300

    Comment lines deleted

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -10,106 +10,57 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-        // Rock => r,
-        // Paper => p,
-        // Scissors =>s
-        int bilgisayarSkor = 0, oyuncuSkor = 0;
-        bas:
+            int computerScore = 0, playerScore = 0;
+            string computerMove = "";
+            string[] rps = { "r", "p", "s" };
+            Random rnd = new Random();
+
+        getInput:
+            Console.Clear();
             Console.WriteLine("******* Rock | Paper | Scissors  ********");
             Console.WriteLine("        Rock     =>   R");
             Console.WriteLine("        Paper    =>   P");
             Console.WriteLine("        Scissors =>   S");
             Console.WriteLine("*****************************************");
-            
-            string oyuncuSecim = Console.ReadLine().ToLower();
-           
-            string bilgisayarSecim = "";
-            
+            Console.Write("Enter your selection: ");
+            string playerMove = Console.ReadLine().ToLower();
+            computerMove = rps[rnd.Next(3)];
 
-            Random rnd = new Random();
-            int x = rnd.Next(0,3);
 
-            string[] xx = { "r", "p", "s" };
-
-            bilgisayarSecim = xx[x];
-
-            if (bilgisayarSecim == oyuncuSecim)
+            if ((computerMove == "r" && playerMove == "s") || (computerMove == "p" && playerMove == "r") || (computerMove == "s" && playerMove == "p"))
             {
-                Console.WriteLine("Berabere");
-                bilgisayarSecim += 0;
-                oyuncuSecim += 0;
-                Console.WriteLine($"{bilgisayarSkor} -- {oyuncuSkor}");
-                
+                computerScore += 10;
+                Console.WriteLine($"Computer : {computerMove}  -----   {playerMove} : You ");
+                Console.WriteLine("Computer Wins !!");
+                Console.WriteLine($"{computerScore} -- {playerScore}");
             }
-            else if (bilgisayarSecim == "r")
+            else if ((playerMove == "r" && computerMove == "s") || (playerMove == "p" && computerMove == "r") || (playerMove == "s" && computerMove == "p"))
             {
-                if (oyuncuSecim == "p")
-                {
-                    Console.WriteLine($"Bilgisayar : {bilgisayarSecim}  -----   {oyuncuSecim} : Siz ");
-                    oyuncuSkor += 10;
-                    Console.WriteLine("Kazanan Oyuncu !!");
-                    Console.WriteLine($"{bilgisayarSkor} -- {oyuncuSkor}");
-                }
-                else if (oyuncuSecim == "s")
-                {
-                    Console.WriteLine($"Bilgisayar : {bilgisayarSecim}  -----   {oyuncuSecim} : Siz ");
-                    bilgisayarSkor += 10;
-                    Console.WriteLine("Kazanan Bilgisayar !!");
-                    Console.WriteLine($"{bilgisayarSkor} -- {oyuncuSkor}");
-                }
+                playerScore += 10;
+                Console.WriteLine($"Computer : {computerMove}  -----   {playerMove} : You ");
+                Console.WriteLine("You Wins !!");
+                Console.WriteLine($"{computerScore} -- {playerScore}");
             }
-            else if (bilgisayarSecim == "p")
-            {
-                if (oyuncuSecim == "r")
-                {
-                    Console.WriteLine($"Bilgisayar : {bilgisayarSecim}  -----   {oyuncuSecim} : Siz ");
-                    bilgisayarSkor += 10;
-                    Console.WriteLine("Kazanan Bilgisayar !!");
-                    Console.WriteLine($"{bilgisayarSkor} -- {oyuncuSkor}");
-                }
-                else if (oyuncuSecim == "s")
-                {
-                    Console.WriteLine($"Bilgisayar : {bilgisayarSecim}  -----   {oyuncuSecim} : Siz ");
-                    oyuncuSkor += 10;
-                    Console.WriteLine("Kazanan Oyuncu !!");
-                    Console.WriteLine($"{bilgisayarSkor} -- {oyuncuSkor}");
-                }
-            }
-            else if (bilgisayarSecim == "s")
-            {
-                if (oyuncuSecim == "r")
-                {
-                    Console.WriteLine($"Bilgisayar : {bilgisayarSecim}  -----   {oyuncuSecim} : Siz ");
-                    oyuncuSkor += 10;
-                    Console.WriteLine("Kazanan Oyuncu !!");
-                    Console.WriteLine($"{bilgisayarSkor} -- {oyuncuSkor}");
-                }
-                else if (oyuncuSecim == "p")
-                {
-                    Console.WriteLine($"Bilgisayar : {bilgisayarSecim}  -----   {oyuncuSecim} : Siz ");
-                    bilgisayarSkor += 10;
-                    Console.WriteLine("Kazanan Bilgisayar !!");
-                    Console.WriteLine($"{bilgisayarSkor} -- {oyuncuSkor}");
-                }
-            }
-
-            Console.Write("Devam etmek istiyor musunuz ? (E/H): ");
-            String cevap = Console.ReadLine().ToLower();
-            if (cevap != "h")
-            {
-                Console.Clear();
-                goto bas;
-            }
-            Console.WriteLine($"Bilgisayar Skor: {bilgisayarSkor} -- Oyuncu skor:  {oyuncuSkor}");
-            if (bilgisayarSkor > oyuncuSkor)
-                Console.WriteLine("Oyunu Bilgisayar Kazandı");
-            else if (bilgisayarSkor < oyuncuSkor)
-                Console.WriteLine("Oyunu Siz Kazandınız");
             else
-                Console.WriteLine("Oyun Berabere");
+            {
+                Console.WriteLine("Draw");
+                Console.WriteLine($"{computerScore} -- {playerScore}");
+            }
 
+            Console.Write("Do you want continue ? (Y/N): ");
+            String cevap = Console.ReadLine().ToLower();
+            if (cevap != "n")
+                goto getInput;
+
+            Console.WriteLine($"Computer Score: {computerScore} --- {playerScore} : Player Score");
+            if (computerScore > playerScore)
+                Console.WriteLine("The Computer Won the Game");
+            else if (computerScore < playerScore)
+                Console.WriteLine("You Won The Game");
+            else
+                Console.WriteLine("Game Draw");
             Console.ReadKey();
-            
+
         }
     }
 }
